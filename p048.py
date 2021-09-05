985314e2433b3e9de761229fe85bec551973e318
tractorboy1 <tractorboy@gmail.com>
2021-09-05 15:13:17 +0100

    Some sort of solution to p048. Very easy

---
+++ p048.py
@@ -0,0 +1,13 @@
+
+class NumberToTheNumber():
+    def __init__(self, number):
+        self.value = number ** number
+
+total = 0
+for n in range(1000):
+    if n % 10 == 0:
+        pass
+    else:
+        item = NumberToTheNumber(n)
+        total += item.value
+print(total)
